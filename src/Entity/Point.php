61bb190cf5f7dc671bdf45d452096a647d6be4e2
unknown <boris.ran7@gmail.com>
2019-09-23 11:27:51 +0200

    formula fix

--- src/Entity/Point.php
+++ src/Entity/Point.php
@@ -17,6 +17,6 @@ class Point
 
     public static function distance($point1, $point2)
     {
-        return sqrt(pow($point2->y - $point1->y, 2) + pow($point2->x - $point1->y, 2));
+        return sqrt(pow($point2->x - $point1->x, 2) + pow($point2->y - $point1->y, 2));
     }
 }
